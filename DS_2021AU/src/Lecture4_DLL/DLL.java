2cc2ab796ab9371f83b4dd29e3f9c94c61c5c91a
saedAsa <saed.asaly@gmail.com>
2021-03-22 11:17:34 +0200

    Douply Linked List

---
+++ DS_2021AU/src/Lecture4_DLL/DLL.java
@@ -0,0 +1,36 @@
+package Lecture4_DLL;
+
+public class DLL {
+	Node head;
+	int size;
+	public DLL() {
+		head=null;
+		size=0;
+		
+	}
+	public void Add(int data) {
+		Node new_Node=new Node(data);
+		new_Node.next=head;
+		if(head!=null) {
+			head.prev=new_Node;
+		}
+		head=new_Node;
+		size++;
+	}
+	public void append(int data) {
+		Node new_Node=new Node(data);
+		if(head==null) {
+			new_Node.prev=null;
+			head=new_Node;
+			size++;
+			return;
+		}
+		Node p=head;
+		while(p.next!=null) {
+			p=p.next;
+		}
+		p.next=new_Node;
+		new_Node.prev=p;
+		size++;
+	}
+}
---
+++ DS_2021AU/src/Lecture4_DLL/Node.java
@@ -0,0 +1,10 @@
+package Lecture4_DLL;
+
+public class Node {
+	int data;
+	Node next,prev;
+	public Node(int data) {
+		this.data=data;
+		next=prev=null;
+	}
+}
